04d415d13e0b7ab8cb022a83515a54a3413a7c16
sarah <sarachamorro@gmail.com>
2019-12-17 14:26:12 -0300

    Eemove extra cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
     def signin(user)
         new_token = User.create_token
         user.remember_token = new_token
-        cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = new_token
     end
 end
